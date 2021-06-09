f7845ee08ccce8ad14ad894b94fd9680d571a77c
ZOrfeas <orfeas.zografos@gmail.com>
2021-06-09 12:30:38 +0300

    Add first draft of SymbolTable and SymbolEntry interfaces

--- sem/symbol.hpp
+++ sem/symbol.hpp
@@ -4,5 +4,60 @@
 #include <vector>
 #include <string>
 
-enum type { TYPE_unknown, TYPE_unit, TYPE_int, TYPE_float, TYPE_bool,
-            TYPE_string, TYPE_char, TYPE_ref, TYPE_arrray, TYPE_function };
+enum class type { TYPE_unknown, TYPE_unit, TYPE_int, TYPE_float, TYPE_bool,
+            TYPE_string, TYPE_char, TYPE_ref, TYPE_array, TYPE_function };
+
+
+/*************************************************************/
+enum class EntryType {
+    VARIABLE, FUNCTION, PARAMETER,
+    CONSTANT, CONSTRUCTOR, TYPE,
+};
+
+class SymbolEntry {
+private:
+public:
+    EntryType eType;
+    virtual ~SymbolEntry() {}
+};
+
+class VariableEntry: public SymbolEntry {
+public:
+    type *t;
+    VariableEntry(const char* name, type *t);
+};
+class ParameterEntry: public SymbolEntry {
+public:
+    type *t;
+    ParameterEntry(const char* name, type *t);
+};
+class FunctionEntry: public SymbolEntry {
+public:
+    type *t;
+    std::vector<ParameterEntry *> parameters;
+    FunctionEntry(const char* name, type *t);
+    void addParam(ParameterEntry *);
+};
+class ConstantEntry: public SymbolEntry {
+public:
+    type *t;
+    ConstantEntry(const char* name, type *t);
+};
+class TypeEntry: public SymbolEntry {};
+class ConstructorEntry: public SymbolEntry {
+public:
+    TypeEntry *customType;
+    ConstructorEntry(const char* name, TypeEntry *customType);
+};
+
+class SymbolTable {
+private:
+public:
+    SymbolTable();
+    void insert(SymbolEntry * entry);
+    void openScope();
+    void closeScope();
+    SymbolEntry* lookup(const char* name, EntryType type,
+                        bool err = true);
+};
+/*************************************************************/
