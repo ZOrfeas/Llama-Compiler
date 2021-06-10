8ae2df10d576c5c9248063d2f6ca62e7f5d8a0af
ZOrfeas <orfeas.zografos@gmail.com>
2021-06-10 17:08:24 +0300

    Add implementations for SymbolTable and SymbolEntry functions, adjust interface

---
+++ sem/symbol.cpp
@@ -0,0 +1,112 @@
+#include "symbol.hpp"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+/*************************************************************/
+/** Common aliases */
+/*************************************************************/
+using tScope = std::map<std::string, SymbolEntry *>;
+using tTable = std::vector<tScope *>;
+using string = std::string;
+
+/*************************************************************/
+/** Helper functions */
+/*************************************************************/
+
+/** Helper function checking existsence of an identifier in a scope */
+bool nameInScope(string name, tScope *scope) {
+    return (scope->find(name) != scope->end());
+}
+
+/*************************************************************/
+/** SymbolTable method implementations */
+/*************************************************************/
+
+SymbolTable::SymbolTable(bool openGlobalScope = true) {
+    Table = new tTable();
+    if (openGlobalScope) {
+        Table->push_back(new tScope());
+    }
+}
+
+SymbolTable::~SymbolTable() {
+    while(closeScope())
+        ; // do nothing while deleting scopes
+    delete Table; // delete the Table at the end
+}
+
+bool SymbolTable::insert(SymbolEntry *entry, bool overwrite = true) {
+    if (nameInScope(entry->name, Table->back()) && !overwrite) {
+            return false;
+    } else {
+        // creates or updates existing
+        (*Table->back())[entry->name] = entry;
+        return true;
+    }
+}
+
+void SymbolTable::openScope() {
+    Table->push_back(new tScope());
+}
+
+bool SymbolTable::closeScope(bool deleteEntries = true) {
+    if (Table->size() != 0) {
+        tScope *poppedScope = Table->back();
+        Table->pop_back();
+        if (deleteEntries) {
+            for (auto &pair: *poppedScope) {
+                delete pair.second;
+            }
+        }
+        delete poppedScope;
+        return true;
+    }
+    return false;
+}
+
+SymbolEntry* SymbolTable::lookup(string name, EntryType type,
+                                 bool err = true) {
+    for (auto it = Table->rbegin(); it != Table->rend(); ++it) {
+        if (nameInScope(name, *it) && (**it)[name]->eType == type) {
+            return (**it)[name];
+        }
+    }
+    std::cout << "SymbolTable:" << std::endl;
+    std::cout << "Symbol " << name << " not found" << std::endl;
+    if (err) { exit(1); }
+    return nullptr;
+}
+
+/*************************************************************/
+/** SymbolEntry method implementations */
+/*************************************************************/
+
+VariableEntry::VariableEntry(std::string n, type *t): SymbolEntry(n,t) {};
+
+ParameterEntry::ParameterEntry(std::string n, type *t): SymbolEntry(n,t) {};
+
+FunctionEntry::FunctionEntry(std::string n, type *t)
+    :SymbolEntry(n,t),
+    parameters(new std::vector<ParameterEntry *>()) {};
+FunctionEntry::~FunctionEntry() { delete parameters; }
+
+ConstantEntry::ConstantEntry(std::string n, type *t): SymbolEntry(n,t) {};
+
+TypeEntry::TypeEntry(std::string n, type *t)
+    :SymbolEntry(n,t),
+    constructors(new std::vector<ConstructorEntry *>()) {};
+TypeEntry::~TypeEntry() { delete constructors; }
+
+ConstructorEntry::ConstructorEntry(std::string n, type *t): SymbolEntry(n,t) {};
+
+
+void ParameterEntry::setFunction(FunctionEntry *f)
+    { funcEntry = f; }
+void FunctionEntry::addParam(ParameterEntry *param)
+    { parameters->push_back(param); }
+void TypeEntry::addConstructor(ConstructorEntry *constr)
+    { constructors->push_back(constr); }
+void ConstructorEntry::setType(TypeEntry *t)
+    { typeEntry = t; }
--- sem/symbol.hpp
+++ sem/symbol.hpp
@@ -3,62 +3,94 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <map>
 
 enum class type { TYPE_unknown, TYPE_unit, TYPE_int, TYPE_float, TYPE_bool,
             TYPE_string, TYPE_char, TYPE_ref, TYPE_array, TYPE_function };
 
 
 /*************************************************************/
+
+/** Enum class with availabel SymbolEntry Types */
 enum class EntryType {
     VARIABLE, FUNCTION, PARAMETER,
     CONSTANT, CONSTRUCTOR, TYPE,
 };
 
+/** Parent class for all possible Symbol Entry types */
 class SymbolEntry {
-private:
 public:
     std::string name;
     EntryType eType;
+    type *ty;
+    SymbolEntry(std::string n, type *t): name(n), ty(t) {};
     virtual ~SymbolEntry() {}
 };
+/*************************************************************/
+// These are necessary to allow circular dependencies
+class FunctionEntry;
+class ParameterEntry;
+
+class ConstructorEntry;
+class TypeEntry;
 
 class VariableEntry: public SymbolEntry {
 public:
-    type *t;
-    VariableEntry(std::string name, type *t);
+    VariableEntry(std::string n, type *t);
+    ~VariableEntry();
 };
 class ParameterEntry: public SymbolEntry {
 public:
-    type *t;
-    ParameterEntry(std::string name, type *t);
+    FunctionEntry *funcEntry;
+    void setFunction(FunctionEntry *f);
+    ParameterEntry(std::string n, type *t);
+    ~ParameterEntry();
 };
 class FunctionEntry: public SymbolEntry {
 public:
-    type *t;
-    std::vector<ParameterEntry *> parameters;
-    FunctionEntry(std::string name, type *t);
-    void addParam(ParameterEntry *);
+    std::vector<ParameterEntry *> *parameters;
+    void addParam(ParameterEntry *param);
+    FunctionEntry(std::string n, type *t);
+    ~FunctionEntry();
 };
 class ConstantEntry: public SymbolEntry {
 public:
-    type *t;
-    ConstantEntry(std::string name, type *t);
+    ConstantEntry(std::string n, type *t);
+    ~ConstantEntry();
 };
-class TypeEntry: public SymbolEntry {};
+class TypeEntry: public SymbolEntry {
+public:
+    std::vector<ConstructorEntry *> *constructors;
+    void addConstructor(ConstructorEntry *constr);
+    TypeEntry(std::string n, type *t);
+    ~TypeEntry();
+};
 class ConstructorEntry: public SymbolEntry {
 public:
-    TypeEntry *customType;
-    ConstructorEntry(std::string name, TypeEntry *customType);
+    TypeEntry *typeEntry;
+    void setType(TypeEntry *t);
+    ConstructorEntry(std::string n, type *t);
+    ~ConstructorEntry();
 };
+/*************************************************************/
 
+/** Symbol Table class */
 class SymbolTable {
-private:
+    /** Pointer to a stack of pointers to maps */
+    std::vector<std::map<std::string, SymbolEntry *> *> *Table;
 public:
-    SymbolTable();
-    void insert(SymbolEntry * entry);
+    /** Constructor can opt out of creating the first scope */
+    SymbolTable(bool openGlobalScope = true);
+    /** Inserts a new symbol entry in the current active scope */
+    bool insert(SymbolEntry *entry, bool overwrite = true);
+    /** Opens a new scope */
     void openScope();
-    void closeScope();
-    SymbolEntry* lookup(const char* name, EntryType type,
+    /** Closes the currently active scope and discards it */
+    bool closeScope(bool deleteEntries = true);
+    /** Lookups up the existence of an identifier as a Symbol Entry of the provided EntryType   
+     *  If not found and err = true, exits, otherwise returns nullptr */
+    SymbolEntry* lookup(std::string name, EntryType type,
                         bool err = true);
+    ~SymbolTable();
 };
 /*************************************************************/
