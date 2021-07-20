1e65b4b015449bbc7aca5e1bd66ad4757751f795
ZOrfeas <orfeas.zografos@gmail.com>
2021-07-20 13:12:16 +0300

    SymbolTable and TypeTable adjustements, corrrections and extensions

--- sem/symbol.cpp
+++ sem/symbol.cpp
@@ -8,8 +8,8 @@
 /** Common aliases */
 /*************************************************************/
 using tScope = std::map<std::string, SymbolEntry *>;
-using tTable = std::vector<tScope *>;
-using string = std::string;
+using sTable = std::vector<tScope *>;
+using std::string;
 
 /*************************************************************/
 /** Helper functions */
@@ -25,32 +25,46 @@ bool nameInScope(string name, tScope *scope) {
 /*************************************************************/
 
 SymbolTable::SymbolTable(bool openGlobalScope = true) {
-    Table = new tTable();
+    Table = new sTable();
     if (openGlobalScope) {
         Table->push_back(new tScope());
     }
 }
-
 SymbolTable::~SymbolTable() {
     while(closeScope())
         ; // do nothing while deleting scopes
     delete Table; // delete the Table at the end
 }
+void SymbolTable::error(string msg, bool crash = true) {
+    std::cout << "SymbolTable:" << std::endl;
+    std::cout << msg << std::endl;
+    if (crash) exit(1);
 
-bool SymbolTable::insert(SymbolEntry *entry, bool overwrite = true) {
+}
+
+SymbolEntry* SymbolTable::insert(SymbolEntry *entry, bool overwrite = true) {
     if (nameInScope(entry->name, Table->back()) && !overwrite) {
-            return false;
+            return nullptr;
     } else {
         // creates or updates existing
         (*Table->back())[entry->name] = entry;
-        return true;
+        return entry;
     }
+}
+SymbolEntry* SymbolTable::lookup(string name, EntryType type,
+                                 bool err = true) {
+    for (auto it = Table->rbegin(); it != Table->rend(); ++it) {
+        if (nameInScope(name, *it) && (**it)[name]->eType == type) {
+            return (**it)[name];
+        }
+    }
+    error("Symbol " + name + " not found", err);
+    return nullptr;
 }
 
 void SymbolTable::openScope() {
     Table->push_back(new tScope());
 }
-
 bool SymbolTable::closeScope(bool deleteEntries = true) {
     if (Table->size() != 0) {
         tScope *poppedScope = Table->back();
@@ -66,32 +80,128 @@ bool SymbolTable::closeScope(bool deleteEntries = true) {
     return false;
 }
 
-SymbolEntry* SymbolTable::lookup(string name, EntryType type,
-                                 bool err = true) {
-    for (auto it = Table->rbegin(); it != Table->rend(); ++it) {
-        if (nameInScope(name, *it) && (**it)[name]->eType == type) {
-            return (**it)[name];
-        }
-    }
-    std::cout << "SymbolTable:" << std::endl;
-    std::cout << "Symbol " << name << " not found" << std::endl;
-    if (err) { exit(1); }
-    return nullptr;
+VariableEntry* SymbolTable::insertVariable(string name, type *t, bool dynamic, bool overwrite = true) {
+    VariableEntry *ve = new VariableEntry(name, dynamic, t);
+    insert(ve, overwrite);
+    return ve;
 }
+ConstantEntry* SymbolTable::insertConstant(string name, type *t, bool overwrite = true) {
+    ConstantEntry *ce = new ConstantEntry(name, t);
+    insert(ce, overwrite);
+    return ce;
+}
+FunctionEntry* SymbolTable::insertFunction(string name, type *t, bool overwrite = true) {
+    FunctionEntry *fe = new FunctionEntry(name, t);
+    insert(fe, overwrite);
+    return fe;
+}
+
+VariableEntry* SymbolTable::lookupVariable(string name, bool err = true) {
+    SymbolEntry *e = lookup(name, EntryType::VARIABLE, err);
+    if (VariableEntry *ve = dynamic_cast<VariableEntry *>(e)) {
+        return ve;
+    } else {
+        error("Internal error. Downcast to requested SymbolEntry subclass failed...");
+    }
+}
+ConstantEntry* SymbolTable::lookupConstant(string name, bool err = true) {
+    SymbolEntry *e = lookup(name, EntryType::CONSTANT, err);
+    if (ConstantEntry *ce = dynamic_cast<ConstantEntry *>(e)) {
+        return ce;
+    } else {
+        error("Internal error. Downcast to requested SymbolEntry subclass failed...");
+    }
+}
+FunctionEntry* SymbolTable::lookupFunction(string name, bool err = true) {
+    SymbolEntry *e = lookup(name, EntryType::FUNCTION, err);
+    if (FunctionEntry *fe = dynamic_cast<FunctionEntry *>(e)) {
+        return fe;
+    } else {
+        error("Internal error. Downcast to requested SymbolEntry subclass failed...");
+    }
+}
+
+
+/*************************************************************/
+/** TypeTable method implementations */
+/*************************************************************/
+
+TypeTable::TypeTable() {
+    Table = new tScope();
+}
+TypeTable::~TypeTable() {
+    for (auto &pair: *Table) {
+        delete pair.second;
+    }
+    delete Table;
+}
+void TypeTable::error(string msg, bool crash = true) {
+    std::cout << "TypeTable:" << std::endl;
+    std::cout << msg << std::endl;
+    if (crash) exit(1);
+}
+
+SymbolEntry* TypeTable::lookup(string name, EntryType type,
+                    bool err = true) {
+    if (nameInScope(name, Table) && (*Table)[name]->eType == type) {
+        return (*Table)[name];
+    }
+}
+SymbolEntry* TypeTable::insert(SymbolEntry *entry, bool overwrite = false) {
+    if (nameInScope(entry->name, Table) && !overwrite) {
+        string msg;
+        if (std::isupper(entry->name[0])) 
+            msg = "Constructor with name ";
+        else
+            msg =  "Type with name ";
+        msg += entry->name + " declared twice";
+        error(msg);
+    } else {
+        // creates or updates existing
+        (*Table)[entry->name] = entry;
+        return entry;
+    }
+}
+
+TypeEntry* TypeTable::insertType(string name, type *t, bool overwrite = false) {
+    TypeEntry* te = new TypeEntry(name, t);
+    insert(te, overwrite);
+    return te;
+}
+ConstructorEntry* TypeTable::insertConstructor(string name, type *t, bool overwrite = false) {
+    ConstructorEntry* ce = new ConstructorEntry(name, t);
+    insert(ce, overwrite);
+    return ce;
+}
+
+TypeEntry* TypeTable::lookupType(string name, bool err = true) {
+    SymbolEntry *e = lookup(name, EntryType::TYPE, err);
+    if (TypeEntry *te = dynamic_cast<TypeEntry *>(e)) {
+        return te;
+    } else {
+        error("Internal error. Downcast to requested SymbolEntry subclass failed...");
+    }
+}
+ConstructorEntry* TypeTable::lookupConstructor(string name, bool err = true) {
+    SymbolEntry *e = lookup(name, EntryType::CONSTRUCTOR, err);
+    if (ConstructorEntry *ce = dynamic_cast<ConstructorEntry *>(e)) {
+        return ce;
+    } else {
+        error("Internal error. Downcast to requested SymbolEntry subclass failed...");
+    }
+}
+
 
 /*************************************************************/
 /** SymbolEntry method implementations */
 /*************************************************************/
 
-VariableEntry::VariableEntry(std::string n, type *t)
-    : SymbolEntry(n,t,EntryType::VARIABLE) {};
-
-ParameterEntry::ParameterEntry(std::string n, type *t)
-    : SymbolEntry(n,t,EntryType::PARAMETER) {};
+VariableEntry::VariableEntry(std::string n, bool dyn, type *t)
+    : SymbolEntry(n,t,EntryType::VARIABLE), dynamic(dyn) {};
 
 FunctionEntry::FunctionEntry(std::string n, type *t)
     :SymbolEntry(n,t,EntryType::FUNCTION),
-    parameters(new std::vector<ParameterEntry *>()) {};
+    parameters(new std::vector<ConstantEntry *>()) {};
 FunctionEntry::~FunctionEntry() { delete parameters; }
 
 ConstantEntry::ConstantEntry(std::string n, type *t)
@@ -106,11 +216,11 @@ ConstructorEntry::ConstructorEntry(std::string n, type *t)
 : SymbolEntry(n,t,EntryType::CONSTRUCTOR) {};
 
 
-void ParameterEntry::setFunction(FunctionEntry *f)
-    { funcEntry = f; }
-void FunctionEntry::addParam(ParameterEntry *param)
+void FunctionEntry::addParam(ConstantEntry *param)
     { parameters->push_back(param); }
-void TypeEntry::addConstructor(ConstructorEntry *constr)
-    { constructors->push_back(constr); }
 void ConstructorEntry::setType(TypeEntry *t)
     { typeEntry = t; }
+void TypeEntry::addConstructor(ConstructorEntry *constr) {
+    constructors->push_back(constr);
+    constr->setType(this);
+}
--- sem/symbol.hpp
+++ sem/symbol.hpp
@@ -11,45 +11,42 @@ enum class type { TYPE_unknown, TYPE_unit, TYPE_int, TYPE_float, TYPE_bool,
 
 /*************************************************************/
 
-/** Enum class with availabel SymbolEntry Types */
+/** Enum class with available SymbolEntry Types */
 enum class EntryType {
-    VARIABLE, FUNCTION, PARAMETER,
-    CONSTANT, CONSTRUCTOR, TYPE,
+    VARIABLE, FUNCTION, CONSTANT, 
+    CONSTRUCTOR, TYPE,
 };
 
+/*************************************************************/
+/**                    SymbolEntry                           */
+/*************************************************************/
 /** Parent class for all possible Symbol Entry types */
 class SymbolEntry {
 public:
-    std::string name;
-    EntryType eType;
-    type *ty;
+    std::string name; // Id associated with SymbolEntry
+    EntryType eType;  // Tracker to be able to Downcast to appropriate subclass
+    type *ty;         // (Placeholder) Will hold pointer to Type Graph of entry
     SymbolEntry(std::string n, type *t, EntryType eType): name(n), ty(t) {};
     virtual ~SymbolEntry() {}
 };
+
+/*************************************************************/
+/**                SymbolEntry SubClasses                    */
 /*************************************************************/
 // These are necessary to allow circular dependencies
-class FunctionEntry;
-class ParameterEntry;
-
 class ConstructorEntry;
 class TypeEntry;
 
 class VariableEntry: public SymbolEntry {
 public:
-    VariableEntry(std::string n, type *t);
+    bool dynamic; // Tracks whether var was dynamically allocated
+    VariableEntry(std::string n, bool dyn, type *t);
     ~VariableEntry();
-};
-class ParameterEntry: public SymbolEntry {
-public:
-    FunctionEntry *funcEntry;
-    void setFunction(FunctionEntry *f);
-    ParameterEntry(std::string n, type *t);
-    ~ParameterEntry();
 };
 class FunctionEntry: public SymbolEntry {
 public:
-    std::vector<ParameterEntry *> *parameters;
-    void addParam(ParameterEntry *param);
+    std::vector<ConstantEntry *> *parameters; // SymbolEntries of Function params
+    void addParam(ConstantEntry *param);      // Adds a function param to this function (doesn't add to SymbolTable)
     FunctionEntry(std::string n, type *t);
     ~FunctionEntry();
 };
@@ -60,37 +57,84 @@ public:
 };
 class TypeEntry: public SymbolEntry {
 public:
-    std::vector<ConstructorEntry *> *constructors;
-    void addConstructor(ConstructorEntry *constr);
+    std::vector<ConstructorEntry *> *constructors; // SymbolEntries of Constructors
+    void addConstructor(ConstructorEntry *constr); // Adds a constructor to this type (doesn't add to TypeTable)
     TypeEntry(std::string n, type *t);
     ~TypeEntry();
 };
 class ConstructorEntry: public SymbolEntry {
 public:
-    TypeEntry *typeEntry;
-    void setType(TypeEntry *t);
+    TypeEntry *typeEntry;       // The TypeEntry this constructor corresponds to
+    void setType(TypeEntry *t); // Sets this Constructor's TypeEntry
     ConstructorEntry(std::string n, type *t);
     ~ConstructorEntry();
 };
 /*************************************************************/
-
-/** Symbol Table class */
+/**                SymbolTable class                         */
+/*************************************************************/
 class SymbolTable {
     /** Pointer to a stack of pointers to maps */
     std::vector<std::map<std::string, SymbolEntry *> *> *Table;
+    /** Looks up the existence of an identifier as a Symbol Entry of the provided EntryType   
+     *  If not found and err = true, exits, otherwise returns nullptr */
+    SymbolEntry* lookup(std::string name, EntryType type,
+                        bool err = true);
+    /** Inserts a new symbol entry in the current active scope */
+    SymbolEntry* insert(SymbolEntry *entry, bool overwrite = true);
+    void error(std::string msg, bool crash = true);
 public:
     /** Constructor can opt out of creating the first scope */
     SymbolTable(bool openGlobalScope = true);
-    /** Inserts a new symbol entry in the current active scope */
-    bool insert(SymbolEntry *entry, bool overwrite = true);
     /** Opens a new scope */
     void openScope();
     /** Closes the currently active scope and discards it */
     bool closeScope(bool deleteEntries = true);
-    /** Lookups up the existence of an identifier as a Symbol Entry of the provided EntryType   
-     *  If not found and err = true, exits, otherwise returns nullptr */
-    SymbolEntry* lookup(std::string name, EntryType type,
-                        bool err = true);
+
+    // Insert wrappers
+    /** insert wrapper for VariableEntries */
+    VariableEntry* insertVariable(std::string name, type *t, bool dynamic, bool overwrite = true);
+    /** insert wrapper for ConstantEntries */
+    ConstantEntry* insertConstant(std::string name, type *t, bool overwrite = true);
+    /** insert wrapper for FunctionEntries */
+    FunctionEntry* insertFunction(std::string name, type *t, bool overwrite = true);
+    // Lookup wrappers
+    /** lookup wrapper for VariableEntries */
+    VariableEntry* lookupVariable(std::string name, bool err = true);
+    /** lookup wrapper for ConstantEntries */
+    ConstantEntry* lookupConstant(std::string name, bool err = true);
+    /** lookup wrapper for FunctionEntries */
+    FunctionEntry* lookupFunction(std::string name, bool err = true);
+
     ~SymbolTable();
 };
+
 /*************************************************************/
+/**                TypeTable class                           */
+/*************************************************************/
+class TypeTable {
+    /** Pointer to a map of name-Entry_pointer pairs*/
+    std::map<std::string, SymbolEntry *> *Table;
+    SymbolEntry *lookup(std::string name, EntryType type,
+                        bool err = true);
+    SymbolEntry* insert(SymbolEntry *entry, bool overwrite = false);
+    void error(std::string msg, bool crash = true);
+public:
+    TypeTable();
+    // Insert wrappers
+    /** insert wrapper for TypeEntries */
+    TypeEntry* insertType(std::string name, type *t, bool overwrite = false);
+    /** insert wrapper for ConstructorEntries */
+    ConstructorEntry* insertConstructor(std::string, type *t, bool overwrite = false);
+    // Lookup wrappers
+    /** lookup wrapper for TypeEntries */
+    TypeEntry* lookupType(std::string name, bool err = true);
+    /** lookup wrapper for ConstructorEntries */
+    ConstructorEntry* lookupConstructor(std::string name, bool err = true);
+    ~TypeTable();
+};
+
+/*************************************************************/
+/**                Table instantiations                      */
+/*************************************************************/
+SymbolTable st;
+TypeTable tt;
